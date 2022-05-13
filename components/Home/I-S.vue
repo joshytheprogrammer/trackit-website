b6ea3e0f41db2073807faa826771607c2db2c64a
joshy111 <jurassicjoshua@protonmail.com>
2022-05-13 11:45:40 +0100

    completed jumbotron and begun i-s-b

--- components/Home/I-S.vue
+++ components/Home/I-S.vue
@@ -1,13 +1,32 @@
 <template>
-  
+  <div class="board-component" :class="{'wide' : !mobile}">
+    <div class="_image">
+
+    </div>
+    <div class="_details">
+      <img src="../../assets/img/SMART.jpg" alt="">
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex"
 export default {
-
+  computed: {
+    ...mapGetters({
+      mobile: 'mobile/mobile',
+    })
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.board-component {
+  padding: 1rem;
 
+  &.wide {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+}
 </style>
--- components/Home/index.vue
+++ components/Home/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
     <hero-component />
+    <board-component />
   </div>
 </template>
 
 <script>
 import HeroComponent from "./Jumbotron.vue"
+import BoardComponent from "./I-S.vue"
 export default {
   components: {
-    HeroComponent
+    HeroComponent,
+    BoardComponent
   }
 }
 </script>
