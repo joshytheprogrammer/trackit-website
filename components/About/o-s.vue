c0c73c7ced34e121193836bd72742dc6789ae225
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-03 16:44:57 +0100

    created our-story layout

--- components/About/o-s.vue
+++ components/About/o-s.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <div class="our-story">
+    <div class="_details">
+      <h1>The Company <br> TrackIT</h1>
+    </div>
+    <div class="_image">
+      <img src="" alt="">
+    </div>
+  </div>
 </template>
 
 <script>
