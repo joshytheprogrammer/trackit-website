a2fe010efb141370b1cfe93276aa473f281dcf10
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-03 17:23:36 +0100

    move components on home to services

--- components/Home/index.vue
+++ components/Home/index.vue
@@ -1,29 +1,17 @@
 <template>
   <div class="home">
     <hero-component />
-    <board-component />
-    <MCE />
-    <Lab />
-    <MIA />
     <FAQ /> 
   </div>
 </template>
 
 <script>
 import HeroComponent from "./Jumbotron.vue"
-import BoardComponent from "./I-S.vue"
-import MCE from "./M-C-E.vue"
-import Lab from "./ict-lab.vue"
-import MIA from "./m-i-a.vue"
 import FAQ from "./faq.vue"
 
 export default {
   components: {
     HeroComponent,
-    BoardComponent,
-    MCE,
-    Lab,
-    MIA,
     FAQ
   }
 }
--- pages/services.vue
+++ pages/services.vue
@@ -1,15 +1,30 @@
 <template>
-  <div style="text-align: center; padding: 1rem;">
-    This is page is under development.
+  <div class="services">
+    <BC />
+    <MCE />
+    <Lab />
+    <MIA />
+    
   </div>
 </template>
 
 <script>
-export default {
+import BC from "~/components/Services/I-S.vue"
+import MCE from "~/components/Services/M-C-E.vue"
+import Lab from "~/components/Services/ict-lab.vue"
+import MIA from "~/components/Services/m-i-a.vue"
 
+
+export default {
+  components: {
+    BC,
+    MCE,
+    Lab,
+    MIA,
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
