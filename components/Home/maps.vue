b6691843b9f5148a6ce98e592342e8dd63cebc3a
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-04 11:56:29 +0100

    added header in h2 tag

--- components/Home/maps.vue
+++ components/Home/maps.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="maps" id="maps" :class="{'wide' : !mobile}">
-    <div class="card">
-      <div class="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.142733117981!2d3.349837015003017!3d6.503609225197974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8d8b74245bed%3A0x98cda859077b56f4!2sTrack%20IT!5e0!3m2!1sen!2sng!4v1654336974311!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div>
-      <div class="info">
-        <h4>Address</h4>
-        <p>40, Oyekan Street Off Ogunlana Drive Surulere, Lagos.</p>
-        <h4>Hours</h4>
-        <p>Monday - Friday</p>
-        <p> 8am - 5pm</p>
-        <h4>Contacts</h4>
-        <p>+234 901 563 0453</p>
-        <p><a href="mailto:info@trackit.com.ng">info@trackit.com.ng</a></p>
+    <div class="_header">
+      <h2>Our Office</h2>
+    </div>
+    <div class="content">
+      <div class="card">
+        <div class="map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.142733117981!2d3.349837015003017!3d6.503609225197974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8d8b74245bed%3A0x98cda859077b56f4!2sTrack%20IT!5e0!3m2!1sen!2sng!4v1654336974311!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="info">
+          <h4>Address</h4>
+          <p>40, Oyekan Street Off Ogunlana Drive Surulere, Lagos.</p>
+          <h4>Hours</h4>
+          <p>Monday - Friday</p>
+          <p> 8am - 5pm</p>
+          <h4>Contacts</h4>
+          <p>+234 901 563 0453</p>
+          <p><a href="mailto:info@trackit.com.ng">info@trackit.com.ng</a></p>
+        </div>
       </div>
     </div>
   </div>
@@ -33,13 +38,24 @@ export default {
 .maps {
   padding: 1rem;
 
-  &.wide {
-    .card {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
+  ._header {
+    margin: 2rem 0;
+    
+    h2 {
+      font-weight: 400;
+      font-size: 36px;
+    }
+  }
 
-      @media screen and (max-width: 900px) {
-        display: block;
+  &.wide {
+    .content {
+      .card {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+
+        @media screen and (max-width: 900px) {
+          display: block;
+        }
       }
     }
   }
@@ -65,8 +81,6 @@ export default {
         color: $dark;
       }
     }
-
-
   }
 }
 </style>
