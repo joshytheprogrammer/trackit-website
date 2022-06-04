a2eb60ab2bb015647a591247a08c461405086505
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-04 15:57:20 +0100

    added second map

--- components/Home/maps.vue
+++ components/Home/maps.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="maps" id="maps" :class="{'wide' : !mobile}">
     <div class="_header">
-      <h2>Our Office</h2>
+      <h2>Our Offices</h2>
     </div>
     <div class="content">
       <div class="card">
@@ -13,9 +13,24 @@
           <p>40, Oyekan Street Off Ogunlana Drive Surulere, Lagos.</p>
           <h4>Hours</h4>
           <p>Monday - Friday</p>
-          <p> 8am - 5pm</p>
+          <p>8am - 5pm</p>
           <h4>Contacts</h4>
           <p>+234 901 563 0453</p>
+          <p><a href="mailto:info@trackit.com.ng">info@trackit.com.ng</a></p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15759.903889839848!2d7.455027980789247!3d9.06595313563224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0b146aaaaaab%3A0x1f731775230620f7!2sTrack%20IT%20Limited!5e0!3m2!1sen!2sng!4v1654354352329!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="info">
+          <h4>Address</h4>
+          <p>Suite 11b, DBM plaza Wuse Zone1, Abuja.</p>
+          <h4>Hours</h4>
+          <p>Monday - Friday</p>
+          <p>8am - 5pm</p>
+          <h4>Contacts</h4>
+          <p>+234 802 317 8729</p>
           <p><a href="mailto:info@trackit.com.ng">info@trackit.com.ng</a></p>
         </div>
       </div>
@@ -47,9 +62,16 @@ export default {
     }
   }
 
+  .content {
+    .card {
+      margin: 1rem 0;
+    }
+  }
+
   &.wide {
     .content {
       .card {
+        margin: 2rem 0;
         display: grid;
         grid-template-columns: 1fr 1fr;
 
