22868e3f62f19d76182ab0d5bc8a18bce8b5a4e2
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-04 11:40:04 +0100

    completed maps on pc

--- components/Home/maps.vue
+++ components/Home/maps.vue
@@ -4,15 +4,16 @@
       <div class="map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.142733117981!2d3.349837015003017!3d6.503609225197974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8d8b74245bed%3A0x98cda859077b56f4!2sTrack%20IT!5e0!3m2!1sen!2sng!4v1654336974311!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
-      <!-- <div class="info">
-        <p>1 Joseph Street, Lagos Island 102273</p>
+      <div class="info">
+        <h4>Address</h4>
+        <p>40, Oyekan Street Off Ogunlana Drive Surulere, Lagos.</p>
         <h4>Hours</h4>
         <p>Monday - Friday</p>
-        <p> 9am - 6pm</p>
+        <p> 8am - 5pm</p>
         <h4>Contacts</h4>
-        <p>08088985577</p>
-        <p><a href="">kingsleyn24@yahoo.com</a></p>
-      </div> -->
+        <p>+234 901 563 0453</p>
+        <p><a href="mailto:info@trackit.com.ng">info@trackit.com.ng</a></p>
+      </div>
     </div>
   </div>
 </template>
@@ -35,12 +36,31 @@ export default {
   &.wide {
     .card {
       display: grid;
-      grid-template-columns: 1fr 2fr;
+      grid-template-columns: 1fr 1fr;
+
+      @media screen and (max-width: 900px) {
+        display: block;
+      }
     }
   }
 
   .map {
     width: 100%;
+  }
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 1rem;
+
+    h6 {
+      padding: 0.9rem 0;
+    }
+
+    p {
+      padding: 0.5rem 0;
+    }
   }
 }
 </style>
