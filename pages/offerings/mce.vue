6b248f6032bfcc78bb90ef371218a81ff6e07eac
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-13 13:10:43 +0100

    improved mce page

--- pages/offerings/mce.vue
+++ pages/offerings/mce.vue
@@ -6,7 +6,27 @@
     </div>
     <div class="details">
       <p>The Microsoft Certified Educator Program is a professional development program that bridges the gap between technology skills and innovative teaching. Educators holding a Microsoft Certified Educator certification can prove they have the skills needed to provide rich, customized learning experiences for their students that incorporate critical 21st century skills using Microsoft tools.</p>
-      
+      <p>
+        Microsoft offers a comprehensive e-learning curriculum, 21CLD, to help educators prepare for this rigorous exam.
+      </p>
+      <p><b>Note:</b> The Microsoft Certified Educator exam assesses whether educators understand how to apply technology tools in the six different content domains, not whether they are proficient in using specific technology tools.</p>
+      <p>
+        <b>Skills measured</b>
+        <ul>
+          <li>Facilitate student collaboration</li>
+          <li>Facilitate skilled communication</li>
+          <li>Facilitate self-regulation</li>
+          <li>Facilitate real-world problem solving and innovation</li>
+          <li>Facilitate student use of Information and Communication Tools (ICT)</li>
+          <li>Use ICT to be an effective educator</li>
+        </ul>
+      </p>
+      <p>Job role: Developer</p>
+      <p>Required exams: 62-193</p>
+      <p>
+        You write. <b>Technology Literacy for Educators.</b>
+        You earn. <b>Microsoft Certified Educator Certificate.</b>
+      </p>
     </div>
   </div>
 </template>
@@ -28,7 +48,6 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    margin-bottom: 2rem;
     border-bottom: 1px solid $dark;
 
     h1 {
@@ -38,6 +57,19 @@ export default {
 
     h1, img {
       margin: 0 1rem;
+    }
+  }
+
+  .details {
+    padding: 1rem;
+
+    p {
+      font-size: 1.1rem;
+      padding: 1.5rem 0;
+
+      ul {
+        padding: 0.5rem 1.5rem;
+      }
     }
   }
 }
